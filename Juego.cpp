fbd00dccff09ff10790dbf63cd8977114348af51
Juan-CRivera <00029324@uca.edu.sv>
2024-07-08 10:49:36 -0600

    V1.0.1

--- Juego.cpp
+++ Juego.cpp
@@ -225,7 +225,7 @@ void Preguntas(string Preguntas[25][9]) //Cambiar el tamaño de la matriz de pre
     Preguntas[14][2] = {"D"};
     Preguntas[14][3] = {"Historia"};
     Preguntas[14][4] = {"Cual es el animal terrestre mas rapido?"};
-    Preguntas[14][5] = {"A) Leo"};
+    Preguntas[14][5] = {"A) Leon"};
     Preguntas[14][6] = {"B) Antilope"};
     Preguntas[14][7] = {"C) Caballo"};
     Preguntas[14][8] = {"D) Guepardo"};
